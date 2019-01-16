7f337f2f7ea62a927908e7a02d12722f43149071
hamyongjae <bbb12388@naver.com>
2019-01-16 23:18:03 +0900

    Algorithm start

--- BOJ_15874.java
+++ BOJ_15874.java
@@ -1,10 +1,23 @@
 package Algorithm;
 
+import java.util.Scanner;
+
 public class BOJ_15874 {
 
 	public static void main(String[] args) {
-		System.out.println("git test");
-		System.out.println("dd");
+		
+		int k,s_length;
+		String s;
+		Scanner scanner = new Scanner(System.in);
+		
+		k = scanner.nextInt();
+		s_length = scanner.nextInt();
+		s = scanner.nextLine();
+		
+		for(int i =0; i<s_length; i++) {
+			
+			
+		}
 	}
 
 }
