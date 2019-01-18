1b04fca9f0f33b9ea17681c6ce43c330545f4350
hamyongjae <bbb12388@naver.com>
2019-01-18 15:11:43 +0900

    Caesar Cipher

--- BOJ_15874.java
+++ BOJ_15874.java
@@ -1,41 +1,46 @@
 package Algorithm;
-
-
 import java.util.Scanner;
 
 public class BOJ_15874 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int k = sc.nextInt();
-		k %=26;
-		int s_length = sc.nextInt();
-		char temp;
-		String s_result = "";
-		sc.nextLine();
-		String s=sc.nextLine();
-		
-		for(int i =0; i<s_length; i++) {
-			
-			temp = s.charAt(i);
-			if('A'<=temp&&temp<='Z') {
-				if((temp+k)<='Z')
-					s_result+=(char)(temp+k);
-				else if('Z'<(temp+k))
-					s_result+=(char)(temp+k-26);
-			}
-			else if('a'<=temp && temp<='z') {
-				if((temp+k)<='z')
-					s_result+=(char)(temp+k);
-				else if('z'<(temp+k))
-					s_result+=(char)(temp+k-26);
-			}
-			else if(temp==',' || temp=='.' ||  temp==' ')
-				s_result +=temp;
-		}
-		System.out.println(s_result);
-	}
 
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt(); k %=26;
+		int s = sc.nextInt();
+		sc.nextLine();
+		char temp;
+		String st=sc.nextLine();
+		StringBuilder sb = new StringBuilder();
+		
+
+		for (int i = 0; i < s; i++) {
+			temp = st.charAt(i);
+			
+			if (temp == ',' || temp == '.' || temp == ' ') {
+				sb.append(temp);
+				continue;
+			}
+			
+			
+			if ('A' <= temp && temp <= 'Z') {
+				temp += k;
+				
+				if('Z'< temp) 
+					temp -=26;	
+			} 
+			
+			else if ('a' <= temp && temp <= 'z') {
+				temp += k;
+				
+				if('z'<temp) 
+					temp -=26;
+			}
+			
+			sb.append(temp);
+		}
+		System.out.println(sb.toString());
+		// N qtaj Wji Gqfhp Ywjj, mtb fgtzy dtz.
+		// N qtaj Wji Gqfhp Ywjj, mtb fgtzy dtz.
+	}
 }
