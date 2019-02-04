697b27c7ffe4c1dfddd725ab57d6c494e4f2041b
hamyongjae <bbb12388@naver.com>
2019-02-04 13:07:04 +0900

    소년 점프

--- BOJ_16469.java
+++ BOJ_16469.java
@@ -5,38 +5,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
-// BOJ_16469
-// 소년 점프
+//BOJ_16469
+//소년 점프
 public class BOJ_16469 {
 
 	static int R, C;
 	static boolean visited[][];
-	static int cnt2[][];
-	static int cnt[][];
 	static int map[][];
-	static int dx[] = { 1, 0, -1, 0 }; // 오른쪽, 아래, 왼쪽, 위쪽, 제자리
+	static int temp[][];
+	static int max[][];
+	static int cnt[][];
+
+	static int dx[] = { 1, 0, -1, 0 };
 	static int dy[] = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 
 		map = new int[R + 1][C + 1];
+		max = new int[R + 1][C + 1];
 		cnt = new int[R + 1][C + 1];
-		cnt2 = new int[R + 1][C + 1];
-		;
 
+		int min=Integer.MAX_VALUE;
+		
 		for (int i = 1; i <= R; i++) {
 			st = new StringTokenizer(br.readLine());
 			String str = st.nextToken();
-			for (int j = 0; j < str.length(); j++)
+			for (int j = 0; j < C; j++)
 				map[i][j + 1] = str.charAt(j) - '0';
 		}
 
@@ -45,47 +46,53 @@ public class BOJ_16469 {
 			int y = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
 
-			BFS(new Point(x, y, 1));
-		}
+			BFS(new Point(x, y));
 
-		int min = Integer.MIN_VALUE;
-		
-		for (int i = 1; i <= R; i++) {
-			for (int j = 1; j <= C; j++) {
-				if (cnt[i][j] != 0 && cnt2[i][j]==3)
-					max = Math.max(cnt[i][j], min);
-			}
-		}
-
-		int temp = 0;
-
-		if(min == Integer.MAX_VALUE) {
-			System.out.println(-1);
-		}
-			
-		else {
-			for (int i = 1; i <= R; i++) {
-				for (int j = 1; j <= C; j++) {
-					if (min == cnt[i][j])
-						temp++;
+			for (int j = 1; j <= R; j++) {
+				for (int k = 1; k <= C; k++) {
+					max[j][k] = Math.max(max[j][k], temp[j][k]);
 				}
 			}
-			System.out.println(min / 3);
-			System.out.println(temp);
 		}
+
+		for (int j = 1; j <= R; j++) {
+			for (int k = 1; k <= C; k++) {
+				if(max[j][k] <min&& map[j][k]==0 && cnt[j][k]==3)
+					min = max[j][k];
+			}
+		}
+		
+		if(min!=Integer.MAX_VALUE) {
+			System.out.println(min);
+			int c=0;
+			for (int j = 1; j <= R; j++) {
+				for (int k = 1; k <= C; k++) {
+					if(min==max[j][k])
+					{
+						c++;
+					}
+				}
+			}
+			System.out.println(c);
+		}
+		else
+			System.out.println(-1);
 	}
 
 	public static void BFS(Point p) {
 		Queue<Point> Q = new LinkedList<>();
 		visited = new boolean[R + 1][C + 1];
+		temp = new int[R + 1][C + 1];
 		visited[p.y][p.x] = true;
-		cnt2[p.y][p.x] += 1;
+		temp[p.y][p.x] = 0;
+		cnt[p.y][p.x] += 1;
 		Q.add(p);
+
 		while (!Q.isEmpty()) {
+
 			int x = Q.peek().x;
 			int y = Q.peek().y;
-			int dist = Q.peek().dist;
-			cnt[y][x] += dist;
+
 			Q.poll();
 
 			for (int i = 0; i < 4; i++) {
@@ -96,10 +103,10 @@ public class BOJ_16469 {
 					continue;
 
 				if (!visited[ny][nx] && map[ny][nx] == 0) {
-
-					Q.add(new Point(nx, ny, dist + 1));
-					visited[ny][nx]=true;
-					cnt2[ny][nx]++;
+					Q.add(new Point(nx, ny));
+					temp[ny][nx] = temp[y][x] + 1;
+					visited[ny][nx] = true;
+					cnt[ny][nx]++;
 				}
 			}
 		}
@@ -107,11 +114,10 @@ public class BOJ_16469 {
 }
 
 class Point {
-	int x, y, dist;
+	int x, y;
 
-	public Point(int x, int y, int dist) {
+	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
-		this.dist = dist;
 	}
 }
