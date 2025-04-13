51499b01b4ca810c5bf080506558f81b86bfcedb
hamyongjae <bbb12388@Naver.com>
2025-04-13 14:30:05 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 200 ms, Memory: 62880 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -23,31 +23,40 @@ public class Main {
 
         System.out.println(min_time - 1);
         System.out.println(cnt);
+
     }
 
     public static void BFS(int start){
         Queue<Integer> qu = new LinkedList<>();
 
         qu.add(start);
-        visited[start] = 1; // 1로 시작해서 나중에 -1 해줌
+
+        visited[start] = 1; // 1로 시작해서 나중에 -1 해줌 (방문하지 않은 배열을 맨처음에 0으로 초기화 해줬기 때문)
 
         while(!qu.isEmpty()){
+
             int now = qu.poll();
 
             if(now == K){
+
                 int time = visited[now];
 
-                if (cnt == 0) {
-                    min_time = time;
+                // 처음 방문 했을 때
+                if(cnt == 0) {
                     cnt = 1;
-                } else if (min_time == time) {
+                    min_time = time;
+                } else if(min_time == time) {
                     cnt++;
                 }
-                continue; // 더 이상 탐색할 필요 없음
+                
+                continue;
+
             }
 
-            for(int next : new int[] {now + 1, now - 1, now * 2}){
+            for(int next : new int[] {now+1, now-1, now * 2}){
                 if(0 <= next && next <= 100000){
+
+                    // 방문하지 않았거나, 같은시간에 도달했을 경우
                     if(visited[next] == 0 || visited[next] == visited[now] + 1){
                         qu.add(next);
                         visited[next] = visited[now] + 1;
