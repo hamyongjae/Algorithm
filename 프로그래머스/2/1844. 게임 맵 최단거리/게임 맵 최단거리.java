55dfa12bae967063f88173d3d7b7b32df0defca0
hamyongjae <bbb12388@Naver.com>
2026-01-31 09:59:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.77 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,52 +2,49 @@ import java.util.*;
 
 class Solution {
     
-    static int[] dy = {1, 0, -1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static int[][] visited;
-    static int row, col;
-    
     public int solution(int[][] maps) {
         
-        row = maps.length;
-        col = maps[0].length;
-        
-        visited = new int[row][col];
         int answer = 0;
         
-        BFS(0, 0, maps);
+        int Y = maps.length;
+        int X = maps[0].length;
         
-        if(visited[row - 1][col - 1] == 0)
-            return -1;
+        int[] dy = {1,0,-1,0};
+        int[] dx = {0,1,0,-1};
         
-        return visited[row - 1][col - 1];
-    }
-    
-    public static void BFS(int y, int x, int[][] maps){
         Queue<int[]> qu = new LinkedList<>();
         
-        qu.add(new int[]{y, x});
+        qu.add(new int[] {0, 0});
         
-        visited[y][x] = 1;
-        
-        while(!qu.isEmpty()) {
-            int cur_y = qu.peek()[0];
-            int cur_x = qu.peek()[1];
+        while(!qu.isEmpty()){
+            
+            int y = qu.peek()[0];
+            int x = qu.peek()[1];
             
             qu.poll();
             
             for(int i = 0; i < 4; i ++){
-                int ny = cur_y + dy[i];
-                int nx = cur_x + dx[i];
                 
-                if(ny < 0 || nx < 0 || row <= ny || col <= nx || visited[ny][nx] != 0)
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                
+                if(nx < 0 || ny < 0 || X <= nx || Y <= ny)
                     continue;
                 
-                if(maps[ny][nx] == 1) {
-                    qu.add(new int[]{ny, nx});
-                    visited[ny][nx] = visited[cur_y][cur_x] + 1;
+                if(maps[ny][nx] == 1){
+                    
+                    maps[ny][nx] = maps[y][x] + 1;
+                    qu.add(new int[] {ny, nx});
+                    
                 }
             }
         }
-    } 
+        
+        if(maps[Y-1][X-1] == 1){
+            return -1;
+        } else {
+            return maps[Y-1][X-1];
+        }
+        
+    }
 }
